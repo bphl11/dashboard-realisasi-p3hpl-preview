a0e86e91a29811100bda96732cffbdf40999eaf2
sibphl11 <asterales.niza@gmail.com>
2026-09-08 19:02:02 +0800

    Copy preview source

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+
+    SHEET_URL: "https://docs.google.com/spreadsheets/d/e/2PACX-1vShdaPwws12pkv75bkQJL9AYjuC_4xjvANknmsoT6HVmgKeQ2DJsLLm5QzbvlKQJeQvqNGzYALsOk5n/pub?gid=1473286966&single=true&output=csv",
+
+    // Endpoint Google Apps Script untuk membaca transaksi INPUT_REALISASI.
+    INPUT_REALISASI_URL: "https://script.google.com/macros/s/AKfycbzdDEvYU0r_9qKeQQZSdcPRtgz01kFsHdMm6PM5nj8K21c28oV4k53co6cCJC8risZScQ/exec"
+
+};
